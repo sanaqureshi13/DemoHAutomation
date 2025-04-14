3551b11f1fdce16aa60199952985276aad5fced3
sanaqureshi13 <sanaaq1305@gmail.com>
2025-04-14 20:49:06 +0530

    commit for firstbranch

--- DemoHAutomation.Web/obj/Debug/net8.0/DemoHAutomation.Web.AssemblyInfo.cs
+++ DemoHAutomation.Web/obj/Debug/net8.0/DemoHAutomation.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoHAutomation.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a140fd445286a57bac38975bd626d7b935188413")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoHAutomation.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoHAutomation.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
